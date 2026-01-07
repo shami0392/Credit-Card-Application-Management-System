244e5f00f534deb1fa3683d153ebb8a004ae9086
ManishKumar Mahato <manishkumarmahato2003@gmail.com>
2026-01-07 11:08:46 +0530

    fix: whitehouse statuic problem

--- CreditCardproject/settings.py
+++ CreditCardproject/settings.py
@@ -126,7 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 
 
